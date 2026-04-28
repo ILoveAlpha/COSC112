ca7223f8490a49c924ce4bfafe26df4f5f1d3d89
AlphaSeeker <ceferinojdolatre@gmail.com>
2026-04-28 15:55:54 -0400

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,39 @@
+public class BinarySearchTree<V> {
+    private BSTNode<V> _root;
+
+    public BinarySearchTree () {
+        _root = null;
+    }
+
+
+    // Insert key-value pair into the BST. The key should be the array index for each respective value.
+    // Hint: You may want to create a private helper method that takes a node as a parameter.
+    public void add(int key, V value) {
+
+    }
+
+    // This should preform inorder traversal using the InOrderTraverse(BSTNode<V> node) helper method.
+    public void InOrderTraverse() {
+        InOrderTraverse(_root);
+        System.out.println();
+    }
+
+    // This should be the recursive inorder traversal method.
+    // Hint: Print keys in sorted order using inorder traversal.
+    private void InOrderTraverse(BSTNode<V> node) {
+
+    }
+
+    // Returns the height of the tree.
+    // Height = number of edges in the longest path from root to a leaf.
+    // An empty tree should return -1.
+    public int height() {
+
+    }
+
+    // Searches for a key in the BST.
+    // Return true if the key exists in the tree, false otherwise.
+    public boolean search(int key) {
+
+    }
+}
